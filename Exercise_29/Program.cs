1d0d8de25caf2891ce1e51955be37bfcb203b44c
Сергей Соколов <sergsok1984@gmail.com>
2022-09-05 10:15:30 +0300

    add Exercise 29

---
+++ Exercise_29/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] RandomArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i ++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+    System.Console.Write($"[{string.Join(", ", RandomArray())}]");
