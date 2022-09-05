a3f6951eb5ff4c06a47884970a71256890d779c7
Сергей Соколов <sergsok1984@gmail.com>
2022-09-05 09:50:29 +0300

    add Exercise 25

---
+++ Exercise_25/Program.cs
@@ -0,0 +1,19 @@
+﻿int numA = ReadInt("Введите число A: ");
+int numB = ReadInt("Введите число B: ");
+ToDegree(numA, numB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
